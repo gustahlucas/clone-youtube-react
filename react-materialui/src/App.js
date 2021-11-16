7ae7b90a540a5e7c61003501de3d9281dde21bfc
gustahlucas <gustavo.lucas@aluno.ufop.edu.br>
2021-11-16 15:24:05 -0300

    add: create initial app

---
+++ react-materialui/src/App.js
@@ -0,0 +1,16 @@
+import {Button, ThemeProvider} from '@material-ui/core';
+
+function App() {
+  return (
+  <ThemeProvider>
+
+  <Button variant="contained" color="primary">
+    Primary
+  </Button> 
+
+  </ThemeProvider>
+  );
+}
+
+
+export default App;
