860ca14ba3002e58fd0951649d4f7dbfe3dbc1b9
gustahlucas <gustavo.lucas@aluno.ufop.edu.br>
2021-11-17 22:02:12 -0300

    merge

--- react-materialui/src/App.js
+++ react-materialui/src/App.js
@@ -1,16 +1,12 @@
-import {Button, ThemeProvider} from '@material-ui/core';
+import { Button, ThemeProvider } from "@material-ui/core";
 
 function App() {
-  return (
-  <ThemeProvider>
-
-  <Button variant="contained" color="primary">
-    Primary
-  </Button> 
-
-  </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <Button variant="contained" color="primary">
+                Primary
+            </Button>
+        </ThemeProvider>
+    );
 }
-
-
 export default App;
